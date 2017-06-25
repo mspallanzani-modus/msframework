a447479dcd4b8ef72ad3e57e798cb4bc62460fdb
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2017-06-26 00:10:01 +0200

    Add user action controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Mslib\Controller;
+
+use Zend\Http\Request;
+
+/**
+ * Class UserController: controller in charge of executing all actions on User Model
+ *
+ * @package Mslib\Controller
+ */
+class UserController extends MsController
+{
+    /**
+     * Returns the user information for the given GET request
+     *
+     * @param Request $request
+     */
+    public function show(Request $request)
+    {
+    }
+
+    /**
+     * Creates a new user for the given POST request
+     *
+     * @param Request $request
+     */
+    public function create(Request $request)
+    {
+    }
+
+    /**
+     * Edits the user for the given PUT request
+     *
+     * @param Request $request
+     */
+    public function edit(Request $request)
+    {
+    }
+
+    /**
+     * Deletes the user for the given DELETE request
+     *
+     * @param Request $request
+     */
+    public function delete(Request $request)
+    {
+    }
+}
