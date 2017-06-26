978c3804ccdc816447a5c5e8030b22625506f262
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2017-06-26 19:24:45 +0200

    The application is now run by the class App

--- index.php
+++ index.php
@@ -2,17 +2,6 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use Monolog\Handler\FirePHPHandler;
-
-// Create the logger
-$logger = new Logger('ms_logger');
-$logger->pushHandler(new StreamHandler(__DIR__.'/var/ms_app.log', Logger::DEBUG));
-$logger->pushHandler(new FirePHPHandler());
-
-$router = new Mslib\Router\Router();
-$router->init('./app/config/routing.json', $logger);
-$router->resolveRequest();
+$app = new \Mslib\App\App();
+$app->init('./app/config/config.json');
 
