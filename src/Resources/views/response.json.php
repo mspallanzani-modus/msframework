adb5ac930e3084d8adb9b66636f0a6f771995edc
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2017-06-26 19:30:59 +0200

    Add a general template to render a json response

---
+++ src/Resources/views/response.json.php
@@ -0,0 +1,6 @@
+{
+    "status"    :"<?php echo $status; ?>",
+    "code"      : "<?php echo $code; ?>",
+    "message"   : "<?php echo $message; ?>",
+    "data"      : <?php echo json_encode($data); ?>
+}
