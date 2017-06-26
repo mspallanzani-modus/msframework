f57e36dd9328567799942245608cd2ce5deb1374
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2017-06-26 19:30:33 +0200

    Add a template to render the User entity

---
+++ src/Resources/views/User/user.json.php
@@ -0,0 +1,6 @@
+{
+  "id"          : "<?php echo $entity->getId(); ?>",
+  "email"       : "<?php echo $entity->getEmail(); ?>",
+  "firstname"   : "<?php echo $entity->getFirstName(); ?>",
+  "lastname"    : "<?php echo $entity->getLastName(); ?>"
+}
