1eb0efb1039d5f6378e79fca86137aef576e38bd
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2017-06-26 19:28:56 +0200

    Add EntityInterface to be used by each model entity

---
+++ src/Model/EntityInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mslib\Model;
+
+/**
+ * Interface EntityInterface
+ *
+ * @package Mslib\Model
+ */
+interface EntityInterface
+{
+    /**
+     * Returns a unique id of the entity
+     *
+     * @return int
+     */
+    public function getId();
+}
