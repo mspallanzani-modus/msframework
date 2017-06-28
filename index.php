7ced575ac95ac18bf3ce4f7306a2c4f4555eabd1
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2017-06-28 13:29:09 +0200

    we return the response to the client here as it was giving problems if done in the App class

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+// Initializing the app
 $app = new \Mslib\App\App();
-$app->init('./app/config/config.json');
+$responseInit = $app->init('./app/config/config.json', './app/config/routing.json');
 
+// If errors occurred during the initialization process, we should get a Response object with the error information
+if ($responseInit instanceof \Zend\Http\PhpEnvironment\Response) {
+    $responseInit->send();
+}
+
+// Configuration was ok: we can now resolve the request
+$response = $app->resolveRequest();
+
+// Sending the response back to the client
+$response->send();
