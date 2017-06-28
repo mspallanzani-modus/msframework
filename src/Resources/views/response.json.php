f60ac0e04fe96b77c6d8e5115e0632ac31ebb85b
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2017-06-28 13:23:37 +0200

    change json structure

--- src/Resources/views/response.json.php
+++ src/Resources/views/response.json.php
@@ -1,5 +1,5 @@
 {
-    "status"    :"<?php echo $status; ?>",
+    "status"    : "<?php echo $status; ?>",
     "code"      : "<?php echo $code; ?>",
     "message"   : "<?php echo $message; ?>",
     "data"      : <?php echo json_encode($data); ?>
